e0f326b338d86f81ac45af0931111eee5f9259f1
Huawei <meelo.k@mail.ru>
2022-02-11 23:26:48 +0300

    Привет.
    Добавил недостающие методы.

--- src/main/java/ru/hogwarts/school/Faculty.java
+++ src/main/java/ru/hogwarts/school/Faculty.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school;
 
+import java.util.Objects;
+
 public class Faculty {
     private long id;
     private String name;
@@ -33,6 +35,20 @@ public class Faculty {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
     }
 
     @Override
--- src/main/java/ru/hogwarts/school/Student.java
+++ src/main/java/ru/hogwarts/school/Student.java
@@ -1,9 +1,17 @@
 package ru.hogwarts.school;
 
+import java.util.Objects;
+
 public class Student {
     private long id;
     private String name;
     private int age;
+
+    public Student(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
     public long getId() {
         return id;
@@ -17,12 +25,6 @@ public class Student {
         return age;
     }
 
-    public Student(long id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
     public void setId(long id) {
         this.id = id;
     }
@@ -33,6 +35,19 @@ public class Student {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 
     @Override
